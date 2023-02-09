b44df539cc80ca7dc71ebe5aa3728fbf0794dd87
dama <mada365@gmail.com>
2023-02-10 04:14:45 +0800

    第一个接口

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
-from fastapi import FastAPI
+from flask import Flask, request
+import openai
 
-app = FastAPI()
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app = Flask(__name__)
+openai.api_key = "your_openai_api_key"
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.route("/")
+def index():
+    return "Hello World!"
+
+
+@app.route("/openai", methods=["POST"])
+def openai_text_completion():
+    prompt = request.form.get("prompt")
+    model = request.form.get("model")
+    completions = openai.Completion.create(
+        engine=model,
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    message = completions.choices[0].text
+    return message
+
+
+if __name__ == "__main__":
+    app.run()
